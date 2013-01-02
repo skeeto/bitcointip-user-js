f9a199106a21f826428e72b05ae6277472477587
Christopher Wellons <mosquitopsu@gmail.com>
2013-01-01 19:31:22 -0500

    Bump to 1.3.3.

--- bitcointip.user.js
+++ bitcointip.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Extra Bitcointip Support on Reddit
 // @description  Add a tipping button (/u/bitcointip) and shrinks verifications
-// @version      1.3.2
+// @version      1.3.3
 // @license      Public Domain
 // @include      http://*.reddit.com/*
 // @exclude      http://*.reddit.com/user/bitcointip
 // ==/UserScript==
 
 /* Changelog:
- * 1.3.2
+ * 1.3.3
+ *    Fix API URL.
  *    Finer tip rejection control
  *    Small bug fixes
  * 1.3.1
