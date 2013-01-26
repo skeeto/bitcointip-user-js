07710e61a336436595990175ad2201bcb5752029
Christopher Wellons <mosquitopsu@gmail.com>
2013-01-25 22:12:29 -0500

    Drop JSONP usage.

--- bitcointip.user.js
+++ bitcointip.user.js
@@ -52,8 +52,8 @@ var botStatusHtml = {
     down: '<span class="status-down">DOWN</span>'
 };
 var api = {
-    gettips: 'http://bitcointip.net/api/gettips.php?callback=?&',
-    gettipped: 'http://bitcointip.net/api/gettipped.php?callback=?&'
+    gettips: 'http://bitcointip.net/api/gettips.php?',
+    gettipped: 'http://bitcointip.net/api/gettipped.php?'
 };
 var icons = {
     verified: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAANCAMAAABFNRROAAAAt1BMVEX///8AAAAAyAAAuwAAwQcAvAcAvwAAwQYAyAUAxAUAxwQAwgQAvAMAxQYAvwYAxQYAxwU5yT060j460j871T89wUE9wkFGokdGu0hIzExJl09JmE9JxExJxE1K1U9K1k5Ll09LmVNMmVNM2FBNmlRRx1NSzlRTqlVUslZU1ldVq1hVrFdV2FhWrFhX21pZqlphrWJh3WRotGtrqm1stW91sXd2t3h5t3urz6zA2sHA28HG3sf4+PhvgZhQAAAAEXRSTlMAARweJSYoLTM0O0dMU1dYbkVIv+oAAACKSURBVHjaVc7XEoIwEIXhFRED1tBUxBaPFSyxK3n/5zIBb/yv9pudnVky2Ywxm345MHkVXByllPm4W24qrLbzdo1sLPPRepc+XlnSIAuz9DQYPtXnkLhUF/ysrndV3CYLRpbg2VtpxFMwfRfEl8IghEPUhB9t9lEQoke6FnzONfpU5kEIoKOn/z+/pREPWTic38sAAAAASUVORK5CYII=",
