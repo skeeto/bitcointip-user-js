c7bf070f27e3341e09e0fc5b0d2a7bea19576256
Christopher Wellons <mosquitopsu@gmail.com>
2013-03-02 13:59:40 -0500

    Bump to 1.4.1.

--- bitcointip.user.js
+++ bitcointip.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Extra Bitcointip Support on Reddit
 // @description  Add a tipping button (/u/bitcointip) and shrinks verifications
-// @version      1.4.0
+// @version      1.4.1
 // @license      Public Domain
 // @include      http*://*.reddit.com/*
 // @exclude      http*://*.reddit.com/user/bitcointip
 // ==/UserScript==
 
 /* Changelog:
+ * 1.4.1
+ *    Bug fixes with balances
  * 1.4.0
  *    Subreddit support indicator
  *    Current balance display
