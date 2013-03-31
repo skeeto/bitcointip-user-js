9c9dcad2b0c724ab475129a84954fef1c261bb41
Christopher Wellons <mosquitopsu@gmail.com>
2013-03-31 01:51:38 -0400

    Mark as unstable.

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Extra Bitcointip Support on Reddit
+// @name         Extra Bitcointip Support on Reddit (unstable)
 // @description  Add a tipping button (/u/bitcointip) and shrinks verifications
-// @version      2.0
+// @version      2.0-unstable
 // @license      Public Domain
 // @include      http*://*.reddit.com/*
 // @exclude      http*://*.reddit.com/user/bitcointip
