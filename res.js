c4b548bc23d4034669799dcb5166096d3ae7a36e
andytuba <andytuba@andytuba.com>
2013-04-04 23:11:55 -0500

    copy in RESUtils.forEachChunked

--- res.js
+++ res.js
@@ -17,6 +17,29 @@ var RESUtils = {
     addCSS: function(css) {
         $(document.body).append($('<style/>').text(css));
     },
+    /*
+    Iterate through an array in chunks, executing a callback on each element.
+    Each chunk is handled asynchronously from the others with a delay betwen each batch.
+    If the provided callback returns false iteration will be halted.
+    */
+    forEachChunked: function(array, chunkSize, delay, call) {
+        if (array == null) return;
+        if (chunkSize == null || chunkSize < 1) return;
+        if (delay == null || delay < 0) return;
+        if (call == null) return;
+        var counter = 0;
+        var length = array.length;
+        function doChunk() {
+            for (var end = Math.min(array.length, counter+chunkSize); counter < end; counter++) {
+                var ret = call(array[counter], counter, array);
+                if (ret === false) return;
+            }
+            if (counter < array.length) {
+                window.setTimeout(doChunk, delay);
+            }
+        }
+        window.setTimeout(doChunk, delay);
+    },
     setCursorPosition: function(form, pos) {
         elem = $(form)[0];
         if (!elem) return;
