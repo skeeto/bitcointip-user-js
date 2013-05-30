fdba5f5fcdb1863fe041b5e4c3a70088be34a33f
Christopher Wellons <mosquitopsu@gmail.com>
2013-05-29 21:57:34 -0400

    Add another example to the footer.

--- footer.js
+++ footer.js
@@ -95,15 +95,17 @@ if (location.pathname === '/r/bitcointip/wiki/preferences') {
 /* ## Test Tips
  *
  * Rejected:
- *   http://www.reddit.com/r/bitcointip/comments/132nhq/t/c7c7iue
+ *   http://www.reddit.com/r/bitcointip/comments/132nhq/_/c7c7iue
  * Rejected flip:
- *   http://www.reddit.com/r/Bitcoin/comments/14i9e7/y/c7dc6w9
+ *   http://www.reddit.com/r/Bitcoin/comments/14i9e7/_/c7dc6w9
  * Combination folding:
- *   http://www.reddit.com/r/bitcointip/comments/13iykn/b/c7dj8ia
+ *   http://www.reddit.com/r/bitcointip/comments/13iykn/_/c7dj8ia
  * Multiple tips to one receiver:
- *   http://www.reddit.com/r/bitcointip/comments/12lmut/c7ny177
+ *   http://www.reddit.com/r/bitcointip/comments/12lmut/_/c7ny177
  * Multiple guilded to one receiver (for comparison):
- *   http://www.reddit.com/r/AdviceAnimals/comments/15mk25/c7ntrcc
+ *   http://www.reddit.com/r/AdviceAnimals/comments/15mk25/_/c7ntrcc
  * Reversed:
- *   http://www.reddit.com/r/IAmA/comments/18tp7t/c8i8qto
+ *   http://www.reddit.com/r/IAmA/comments/18tp7t/_/c8i8qto
+ * Fun tip to ignore:
+ *   http://www.reddit.com/r/gaming/comments/18ihmm/_/c8f54l4
  */
