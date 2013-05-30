51cc51df856fc4b9c528867ca499924093542460
Christopher Wellons <mosquitopsu@gmail.com>
2013-05-29 22:04:04 -0400

    Bump to 2.0.1.
    Oops, add missing changelog.js file, too.

---
+++ changelog.js
@@ -0,0 +1,68 @@
+/* ## ChangeLog
+ * * 2.0.1
+ *   * Support for new /u/bitcointip keyword
+ *   * Small fixes
+ * * 2.0.0
+ *   * Refactor the entire script for RES
+ * * 1.5.2
+ *   * Bug fix
+ * * 1.5.1
+ *   * Bug fixes
+ *   * Tweaked the preferences page styling
+ * * 1.5.0
+ *   * Added a preferences page
+ *   * Tip button does both public and private tipping
+ *   * Switch to HTTPS for API
+ * * 1.4.5
+ *   * Fix currency display precision
+ * * 1.4.4
+ *   * Fix cancelled icon bug
+ *   * Update to latest tip regexp
+ * * 1.4.3
+ *   * Fix subreddit case-sensitivity bug
+ *   * Adjusted "pending" icon color
+ * * 1.4.2
+ *   * Bug fixes with balances
+ * * 1.4.0
+ *   * Subreddit support indicator
+ *   * Current balance display
+ *   * New icons for richer status indicators
+ *   * Support for reddit https (https everywhere).
+ * * 1.3.5
+ *   * Drop JSONP for CORS to increase safety.
+ *   * Attempt to link to source tip from tipped icons.
+ * * 1.3.4
+ *   * Inject the bot status into r/bitcointip's sidebar
+ * * 1.3.3
+ *   * Fix API URL.
+ *   * Finer tip rejection control
+ *   * Small bug fixes
+ * * 1.3.1
+ *   * A tip button on posts
+ *   * Verification icons on posts
+ *   * Small display tweaks
+ * * 1.3.0
+ *   * New verification and rejection markings
+ *   * Use the new gettipped API
+ *   * All new jQuery reddit plugin
+ * * 1.2.1
+ *   * More efficient validation marking
+ *   * Fixed status names
+ *   * Run on all of reddit, including user pages
+ * * 1.2
+ *   * Use the new bitcointip API to get status information
+ *   * https://github.com/NerdfighterSean/bitcointip/blob/master/src/api/
+ * * 1.1.4
+ *   * Don't hide verification when it has children
+ *   * Don't hide verification when it's the URL target
+ * * 1.1.3
+ *   * Fix case insensitivity in regex
+ * * 1.1.2
+ *   * Display tipping amount
+ * * 1.1.1
+ *   * Support the full tipping syntax
+ * * 1.1
+ *   * Hide verification posts, inlining the verification
+ * * 1.0
+ *   * Add a "tip bitcoins" button.
+ */
